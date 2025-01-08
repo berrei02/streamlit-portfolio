c08d87ee5e01f06ae8ea7a88c687f866fc7d1d0b
Stefan <berreiter@hotmail.de>
2025-01-08 22:23:22 +0100

    tennis mvp with bigquery loading

--- app.py
+++ app.py
@@ -1,26 +1,17 @@
 import streamlit as st
 
-st.set_page_config(page_title="Stefan Berreiter")
-
 about_page = st.Page("content/about.py", title="About Stefan")
 
-project_foo = st.Page(
-    "content/foo.py", title="Foo App", icon=":material/dashboard:")
+tennis_dashboard = st.Page(
+    "content/tennis_dashboard.py", title="Tennis Analytics", icon=":material/dashboard:")
 
-project_bar = st.Page("content/bar.py", title="Bar app", icon=":material/bug_report:")
+next_project = st.Page("content/next.py", title="Upcoming ...", icon=":material/bug_report:")
 
 
 pg = st.navigation(
     {
         "About": [about_page],
-        "Apps": [project_foo, project_bar],
+        "Apps": [tennis_dashboard, next_project],
     }
 )
-
-st.write("# Welcome")
-
-main_page_text = """
-This will become my portfolio Website.
-"""
-
-st.write(main_page_text)
+pg.run()
---
+++ content/tennis_dashboard.py
@@ -0,0 +1,57 @@
+import streamlit as st
+from google.oauth2 import service_account
+from google.cloud import bigquery
+import pandas as pd
+import plotly.express as px
+
+
+# Create API client.
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"]
+)
+client = bigquery.Client(credentials=credentials)
+
+# Perform query.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+@st.cache_data(ttl=600)
+def run_query(query):
+    query_job = client.query(query)
+    rows_raw = query_job.result()
+    # Convert to list of dicts. Required for st.cache_data to hash the return value.
+    rows = [dict(row) for row in rows_raw]
+     # Convert rows_raw to a list of dictionaries    
+    df = pd.DataFrame(rows)
+    return df
+
+st.write("""
+# Tennis Match Dashboard
+Displaying some simple stats
+## Technologies
+- Storage: Google BigQuery
+- Data Visualization: Streamlit & Plotly
+""")
+
+
+top_players_cnt = st.slider("Show Top", 0, 100, 10)
+st.write(f"Showing the {top_players_cnt} best male Tennis players in 2024.")
+
+df_100 = run_query("select *, case when lost_matches = 0 then null else round(won_matches / lost_matches, 2) end as win_lose_ratio from `streamlit-demo-db.tennis.atp_stats` LIMIT 100")
+
+df = df_100.head(top_players_cnt)
+
+# Create scatter plot
+fig = px.scatter(
+    df,
+    x='won_matches',
+    y='lost_matches',
+    color='win_lose_ratio', 
+    hover_data=['player_name']
+)
+
+# Display in Streamlit
+st.plotly_chart(fig)
+
+
+# Print results.
+with st.expander("Detail data"):
+    st.table(df)
