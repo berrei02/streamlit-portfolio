4b882e8be2ce320959dc8e45e6d68e5d828ff71f
Stefan <berreiter@hotmail.de>
2025-01-09 21:36:06 +0100

    updates to tennis dashboard

--- content/tennis_dashboard.py
+++ content/tennis_dashboard.py
@@ -3,6 +3,7 @@ from google.oauth2 import service_account
 from google.cloud import bigquery
 import pandas as pd
 import plotly.express as px
+import datetime
 
 
 # Create API client.
@@ -23,35 +24,71 @@ def run_query(query):
     df = pd.DataFrame(rows)
     return df
 
-st.write("""
-# Tennis Match Dashboard
-Displaying some simple statistics for ATP tennis matches in 2024.
-Overall it seems that Jannik Sinner had an awesome year. 
-Data was pulled from GitHub and is stored, transformed and served via Google BigQuery.
-## Technologies
-- Storage: Google BigQuery
-- Data Visualization: Streamlit & Plotly
+st.write("""# Tennis Match Dashboard""")
+
+with st.expander("Project Description"):
+    st.write("""
+    Displaying some simple statistics for ATP tennis matches in 2024.
+    Overall it seems that Jannik Sinner had an awesome year. 
+    Data was pulled from GitHub and is stored, transformed and served via Google BigQuery.
+    ## Technologies
+    - Storage: Google BigQuery
+    - Data Visualization: Streamlit & Plotly
+    """)
+
+
+st.divider()
+
+with st.container(border=True):
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        top_players_cnt = st.slider("Show Top", 0, 100, 10)
+
+    with col2:
+        start_date = st.date_input("Start Date", datetime.date(2023, 1, 1))
+    
+    with col3:
+        end_date = st.date_input("End Date", datetime.date.today())
+
+
+df = run_query(f"""
+select player_name
+     , count(1) as matches_cnt
+     , countif(player_is_winner) as matches_won
+     , countif(not player_is_winner) as matches_lost
+  from `streamlit-demo-db.tennis.match_history`
+ where tourney_date between '{start_date}' and '{end_date}'
+ group by 1
+ order by 2 desc
 """)
 
+df_size = df.shape[0]
 
-top_players_cnt = st.slider("Show Top", 0, 100, 10)
-st.write(f"Showing the {top_players_cnt} best male Tennis players in 2024.")
+if df_size == 0: 
+    st.write("Please change parameters, no results are returned.")
+else:
+    df = df.head(top_players_cnt)
+    st.write(f"Showing the {df_size} best male Tennis players from {start_date} to {end_date}.")
 
-df_100 = run_query("select *, case when lost_matches = 0 then null else round(won_matches / lost_matches, 2) end as win_lose_ratio from `streamlit-demo-db.tennis.atp_stats` LIMIT 100")
+    # Create scatter plot
+    fig = px.scatter(
+        df,
+        x='matches_won',
+        y='matches_lost',
+        #color='win_lose_ratio', 
+        hover_data=['player_name'],
+        text="player_name"
+    )
 
-df = df_100.head(top_players_cnt)
+    # Customize the text position and size of points
+    fig.update_traces(
+        textposition='top center',  # Position the text labels
+        marker=dict(size=10)  # Set a constant size for scatter points
+    )
 
-# Create scatter plot
-fig = px.scatter(
-    df,
-    x='won_matches',
-    y='lost_matches',
-    color='win_lose_ratio', 
-    hover_data=['player_name']
-)
-
-# Display in Streamlit
-st.plotly_chart(fig)
+    # Display in Streamlit
+    st.plotly_chart(fig)
 
 
 # Print results.
